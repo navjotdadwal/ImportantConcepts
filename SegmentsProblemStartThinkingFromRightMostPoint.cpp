b123ea6b1b6ea1a171f5c12e30f8adca76fc8aaa
navjotdadwal <59385365+navjotdadwal@users.noreply.github.com>
2022-10-20 09:47:26 +0530

    Create SegmentsProblemStartThinkingFromRightMostPoint.cpp

---
+++ SegmentsProblemStartThinkingFromRightMostPoint.cpp
@@ -0,0 +1,13 @@
+/*
+SegmentsProblemStartThinkingFromRightMostPoint
+
+Problem 1 -> https://codeforces.com/contest/1249/problem/D2
+
+                               __________
+            _____________
+   _____________
+   _______
+1  2  3  4  5  6  7  8  9  10  11  12  13
+
+some problems need to be thought from the right most point i.e. 2
+*/
