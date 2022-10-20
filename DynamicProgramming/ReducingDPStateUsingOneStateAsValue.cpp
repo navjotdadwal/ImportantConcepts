b1d8ceb2aa18c357268f0656d2d390477d510ad8
navjotdadwal <59385365+navjotdadwal@users.noreply.github.com>
2022-10-20 16:08:49 +0530

    Create ReducingDPStateUsingOneStateAsValue.cpp

---
+++ DynamicProgramming/ReducingDPStateUsingOneStateAsValue.cpp
@@ -0,0 +1,9 @@
+/*
+Problem -> https://www.codechef.com/submit/RBFLOWERS?tab=statement
+
+Knapsack-style dynamic programming
+
+kicking out one state of our dp
+We can achieve that by a relatively common trick: turn the removed state into the value of the dp!
+
+*/
